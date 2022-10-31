e78c3ebc99c24b20342c9d23e400108eb4558d39
gloriaguilhermeisrael <116736996+gloriaguilhermeisrael@users.noreply.github.com>
2022-10-31 14:06:57 -0300

    Update funções.h

--- funções.h
+++ funções.h
@@ -1,5 +1,30 @@
 #ifndef funções.h
 #define funções.h
+
+typedef struct{
+	char nome[30];
+	char email[50];
+	struct{
+		char rua[20];
+		int numero;
+		char complemento[30];
+		char bairro;
+		char cep[10];
+		char cidade[15];
+		char estado[20];
+		char pais[12];
+	}endereco;
+	struct{
+		int ddd;
+		int telefone;	
+	}telefone;
+	struct{
+		int dia;
+		int mes;
+		int ano;
+	}data;
+}dados;
+
 PrimeiroNome(char nome);
 mesAniversario(int mes);
 adicionarPessoa();
